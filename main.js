ee3c7aae02f5f56667655eb7b700f445970378d3
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-10-24 15:13:23 +0200

    add countdown

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log ("Memorizza i numeri")
+console.log("Memorizza i numeri")
+
+const playButton = document.getElementById('playButton')
+const countDown = document.getElementById('countdown')
+let timer
+
+
+playButton.addEventListener('click', function () {
+  playButton.disabled = true
+
+  // Gestiamo il timer
+  count = 30
+  countDown.innerText = count
+
+  // incremento il tempo
+  timer = setInterval(() => {
+    if (count > 0){
+      countDown.innerText = --count;
+    } else
+    countDown.innerText = 0
+  }, 1000)
+
+ 
+
+})
+
