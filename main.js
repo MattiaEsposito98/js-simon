7ebfab5bc6c030dc3845760d7313e29ce5c04be2
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-10-25 14:48:52 +0200

    add correzione

--- main.js
+++ main.js
@@ -1,124 +1,37 @@
-console.log("Memorizza i numeri")
+console.log ("Memorizza i numeri")
+//elementi del dom
+const countDownElement = document.getElementById('countdown');
+const numbersListElement = document.getElementById('numbers-list');
+const instructionsElement = document.getElementById('instructions');
+const answersFormElement = document.getElementById('answers-form');
+const inputsElement = document.querySelectorAll('#input-group input');
+const messageElement = document.getElementById('message');
+const buttonElement = document.querySelector('button');
 
-const playButton = document.getElementById('avvia')
-const sendButton = document.getElementById('invia')
-const countDown = document.getElementById('countdown')
+//variabili di gioco
+const numbersToGuess = 5; //numeri da indovinare
+const minValue = 1;
+const maxValue = 50;
+let playTime = 5; //secondi di gioco per ricordare i numeri
 
-let randomNumber1El = document.getElementById('random_num1') // HTMLElement
-let randomNumber2El = document.getElementById('random_num2')
-let randomNumber3El = document.getElementById('random_num3')
-let randomNumber4El = document.getElementById('random_num4')
-let randomNumber5El = document.getElementById('random_num5')
-let displayInput = document.getElementById('input-group')
-let formInput = document.getElementById("answers-form")
-let listNumberRandom = document.getElementById('numbers-list')
-let messaggio = document.getElementById('message')
-let timer
-let randomArray = []
-let userArray = []
-let totArray = []
+// Generare TOT numeri random
+const numbers = generateRandomNumbersArray(numbersToGuess, minValue, maxValue);
+// Inseriamo i numeri in pagina
+appendElements(numbersListElement, numbers);
 
+// # Conto alla rovescia
+countDownElement.innerText = playTime;
 
-
-playButton.addEventListener('click', function () {
-  playButton.disabled = true
-  sendButton.disabled = false
-
-  //Num1
-  const randomNum = (Math.floor(Math.random() * 50) + 1) // Number
-  randomNumber1El.innerText = randomNum
-  console.log(randomNumber1El)
-
-  //num:2
-  const randomNum2 = (Math.floor(Math.random() * 50) + 1)
-  randomNumber2El.innerHTML = randomNum2
-  console.log(randomNumber2El)
-
-  //Num3
-  const randomNum3 = (Math.floor(Math.random() * 50) + 1)
-  randomNumber3El.innerHTML = randomNum3
-  console.log(randomNumber3El)
-
-  //Num4
-  const randomNum4 = (Math.floor(Math.random() * 50) + 1)
-  randomNumber4El.innerHTML = randomNum4
-  console.log(randomNumber4El)
-
-  //Num5
-  const randomNum5 = (Math.floor(Math.random() * 50) + 1)
-  randomNumber5El.innerHTML = randomNum5
-  console.log(randomNumber5El)
-
-  // Push elementi nell'array
-  randomArray.push(randomNum, randomNum2, randomNum3, randomNum4, randomNum5)
-  console.log(randomArray)
-
-
-
-  // Gestiamo il timer
-  count = 5
-  countDown.innerText = count
-
-  // incremento il tempo
-  timer = setInterval(() => {
-
-    if (count > 0) {
-      countDown.innerText = --count
-
-    } else if (count < 1) {
-      formInput.classList.remove('d-none')
-      listNumberRandom.classList.add('d-none')
-    }
-
-    else {
-      clearInterval(timer)
-    }
-  }, 1000)
-
-})
-
-
-
-// 2 bottone Invia
-sendButton.addEventListener('click', function () {
-  //checkNumber()
-
-  sendButton.disabled = true
-  playButton.disabled = false
-
-  const inputUser1 = document.getElementById("number1").value
-  const inputUser2 = document.getElementById("number2").value
-  const inputUser3 = document.getElementById("number3").value
-  const inputUser4 = document.getElementById("number4").value
-  const inputUser5 = document.getElementById("number5").value
-
-  userArray = [inputUser1, inputUser2, inputUser3, inputUser4, inputUser5]
-  console.log(userArray)
-
-  score = 0
-
-  // ciclo per vedere se il numero e corretto
-  for (let i = 0; i < userArray.length; i++) {
-
-    if (randomArray.includes(userArray[i])) {
-      score++ 
-      totArray.push(usersNumber);
-    }
+const playInterval = setInterval(function () {
+  playTime = playTime - 1;
+  countDownElement.innerText = playTime;
+  if (playTime === 0) {
+    clearInterval(playInterval);
+    numbersListElement.classList.add('d-none');
+    instructionsElement.innerText = 'Quanti numeri riesci a ricordare?';
+    answersFormElement.classList.remove('d-none');
   }
-  messaggio.innerText = `Hai indovinato ${score} numeri`
+}, 1_000);
 
-})
-
-
-// function checkNumber() {
-
-// }
-
-
-
-
-
-
-
-
-
+//giochiamo
+answersFormElement.addEventListener('submit', playGame);
